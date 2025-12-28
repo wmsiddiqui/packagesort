bc51b13598b9c584d157015e668d1666e3ba97b3
wmsiddiqui <wmsiddiqui.6@gmail.com>
2025-12-27 18:00:10 -0600

    Added command line arguments

--- package_sorter.py
+++ package_sorter.py
@@ -1,3 +1,5 @@
+import sys
+
 class PackageSorter:
     HEAVY_MASS_MINIMUM = 20
     BULKY_DIMENSION_MINIMUM = 150
@@ -7,7 +9,7 @@ class PackageSorter:
     REJECTED = "REJECTED"
 
 
-    def sort_package(self, width: int, height: int, length: int, mass: int) -> str:
+    def sort_package(self, width: float, height: float, length: float, mass: float) -> str:
         if width <= 0 or height <= 0 or length <= 0:
             raise ValueError("Dimensions must be greater than 0")
         if mass <= 0:
@@ -22,17 +24,23 @@ class PackageSorter:
             return PackageSorter.SPECIAL
         return PackageSorter.STANDARD
 
-    def _is_bulky(self, width: int, height: int, length: int) -> bool:
+    def _is_bulky(self, width: float, height: float, length: float) -> bool:
         if width * height * length >= self.BULKY_VOLUME_MINIMUM:
             return True
         if width >= PackageSorter.BULKY_DIMENSION_MINIMUM or height >= PackageSorter.BULKY_DIMENSION_MINIMUM or length >= PackageSorter.BULKY_DIMENSION_MINIMUM:
             return True
         return False
 
-    def _is_heavy(self, mass: int) -> bool:
+    def _is_heavy(self, mass: float) -> bool:
         return mass >= PackageSorter.HEAVY_MASS_MINIMUM
 
 if __name__ == "__main__":
     # This block ensures the code runs only when executed as a script
     sorter = PackageSorter()
-    print(sorter.sort_package(10, 20, 140, 15))
+
+    if len(sys.argv) == 1:
+        print(sorter.sort_package(10, 20, 140, 15))
+    elif len(sys.argv) != 5:
+        raise ValueError("Must provide 4 arguments")
+    else:
+        print(sorter.sort_package(float(sys.argv[1]), float(sys.argv[2]), float(sys.argv[3]), float(sys.argv[4])))
